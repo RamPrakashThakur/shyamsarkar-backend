a6b8a11d554d13e0e9fae4662c3fff9cdec00eaa
RAMPRAKASHTHAKUR <ramprakashthakur@RAMPRAKASHTHAKURs-MacBook-Air.local>
2026-04-09 19:52:02 +0530

    firebase config using env variable

--- src/main/java/com/shyamsarkar/buildingmaterials/config/FirebaseConfig.java
+++ src/main/java/com/shyamsarkar/buildingmaterials/config/FirebaseConfig.java
@@ -7,7 +7,10 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
@@ -15,11 +18,17 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            String json = System.getenv("GOOGLE_CREDENTIALS");
 
+            if (json == null || json.isEmpty()) {
+                throw new RuntimeException("GOOGLE_CREDENTIALS not found");
+            }
+
+            InputStream stream = new ByteArrayInputStream(
+                    json.getBytes(StandardCharsets.UTF_8)
+            );
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(stream))
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
