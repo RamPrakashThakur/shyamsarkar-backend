2b8cdcac507d3aaebc143fb2bec5849e65dc84bd
RAMPRAKASHTHAKUR <ramprakashthakur@RAMPRAKASHTHAKURs-MacBook-Air.local>
2026-06-14 18:53:07 +0530

    Add driver location logging

--- src/main/java/com/shyamsarkar/buildingmaterials/controller/DriverTrackingController.java
+++ src/main/java/com/shyamsarkar/buildingmaterials/controller/DriverTrackingController.java
@@ -2,6 +2,9 @@ package com.shyamsarkar.buildingmaterials.controller;
 
 import com.shyamsarkar.buildingmaterials.dto.DriverLocationDto;
 import lombok.RequiredArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -12,8 +15,18 @@ public class DriverTrackingController {
 
     private final SimpMessagingTemplate messagingTemplate;
 
+    private static final Logger logger =
+        LoggerFactory.getLogger(DriverTrackingController.class);
+
     @MessageMapping("/driver/location")
     public void receiveLocation(DriverLocationDto dto) {
+
+        
+
+logger.info("Driver location received: {} {} {}",
+        dto.getOrderId(),
+        dto.getLat(),
+        dto.getLng());
 
         messagingTemplate.convertAndSend(
                 "/topic/location/" + dto.getOrderId(),
